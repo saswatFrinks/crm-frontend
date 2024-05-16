11c9a9f5c91a4de74f2637e90a4f996cd2a39b1c
navendrafrinksai <navendra.agrawal@frinks.ai>
2024-05-16 11:25:20 +0530

    added new SVG icons and added logout feature in dropdown

--- src/shared/ui/UserDropdown.jsx
+++ src/shared/ui/UserDropdown.jsx
@@ -1,15 +1,64 @@
 import storageService from '@/core/storage';
-import { IoIosArrowDown } from 'react-icons/io';
+import ChevronUp from '../icons/ChevronUp';
+import Label from './Label';
+import { useState } from 'react';
+import ChevronDown from '../icons/ChevronDown';
+// import User from '../icons/User';
+import Logout from '../icons/Logout';
+import { TOKEN } from '@/core/constants';
+import { useNavigate } from 'react-router-dom';
 export default function UserDropdown() {
   const name = JSON.parse(storageService.get('user'))?.name;
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleOpen = () => {
+    setOpenDropdown(prev => !prev);
+  }
+
+  const handleLogout = () => {
+    const cookie = `${TOKEN}=;path=/`;
+    document.cookie = cookie;
+    navigate('/login')
+  }
+
+  const dropdownMenu = [
+    // {
+    //   title: 'Account',
+    //   icon: <User />
+    // },
+    {
+      title: 'Log Out',
+      icon: <Logout />,
+      onClick: handleLogout
+    }
+  ]
 
   return (
-    <div className="flex w-full cursor-pointer items-center justify-between gap-4 px-2">
-      <div className="flex h-10 w-10 items-center justify-center rounded-md bg-gray-200 font-semibold">
-        {name ? name[0].toUpperCase() : ''}
+    <div className="flex flex-col">
+      {openDropdown && (
+        <div className="flex flex-col items-center justify-center rounded-lg bg-gray-100 m-2 overflow-hidden">
+          {dropdownMenu.map(item => (
+            <div 
+              className="flex items-center justify-evenly py-2 px-4 cursor-pointer hover:bg-gray-200" 
+              style={{width: '100%'}}
+              onClick={item.onClick}
+            >
+              {item.icon}
+              {item.title}
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="flex w-full cursor-pointer items-center justify-between gap-4 px-2">
+        <div className="flex h-10 w-10 items-center justify-center rounded-md bg-gray-200 font-semibold">
+          {name ? name[0].toUpperCase() : ''}
+        </div>
+        <div className="whitespace-nowrap grow">{name}</div>
+        <Label onClick = {toggleOpen}>
+          {openDropdown ? <ChevronDown /> : <ChevronUp />}
+        </Label>
       </div>
-      <div className=" grow">{name}</div>
-      <IoIosArrowDown />
     </div>
   );
 }
