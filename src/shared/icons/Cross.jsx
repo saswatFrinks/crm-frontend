4acb67198715a8bb28153e5984d2963c67460c94
navendrafrinksai <navendra.agrawal@frinks.ai>
2024-05-28 12:56:57 +0530

    Changed the design for error modal

---
+++ src/shared/icons/Cross.jsx
@@ -0,0 +1,20 @@
+const Cross = () => {
+  return (
+    <svg 
+      width="18" 
+      height="18" 
+      viewBox="0 0 18 18" 
+      fill="none" 
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path 
+        d="M17 1L1 17M1 1L17 17" 
+        stroke="#F73B2F" 
+        stroke-width="4" 
+        stroke-linecap="round"
+      />
+    </svg>
+  )
+}
+
+export default Cross
--- src/shared/ui/ErrorModal.jsx
+++ src/shared/ui/ErrorModal.jsx
@@ -3,16 +3,20 @@ import { ModalBody, ModalHeader } from './Modal'
 import Button from './Button'
 import { useRecoilState } from 'recoil'
 import { modalAtom } from '../states/modal.state'
+import Cross from '../icons/Cross'
 
 const ErrorModal = ({error}) => {
   const [open, setOpen] = useRecoilState(modalAtom);
 
   return (
     <>
-      <ModalHeader>Error</ModalHeader>
+      <ModalHeader></ModalHeader>
       <ModalBody>
-        <div className="text-red-500">{error}</div>
-        <div className='text-right mt-2'>
+        <div className="flex flex-col items-center gap-4 my-2">
+          <div className="border-8 rounded-full bg-white p-4">
+            <Cross />
+          </div>
+          <div className="text-center text-lg font-medium">{error}</div>
           <Button 
             color='primary'
             fullWidth={false}
