b85a116d7eae364a907582cbb80daea09b5f8124
navendrafrinksai <navendra.agrawal@frinks.ai>
2024-06-25 11:01:09 +0530

    Crosshair fixed

--- src/modules/project/assembly/components/Crosshair.jsx
+++ src/modules/project/assembly/components/Crosshair.jsx
@@ -1,14 +1,12 @@
 import { Line } from 'react-konva';
 
-function Crosshair({ x, y, width, height, offset }) {
-  const actualOffset = offset || {x: 0, y: 0}
-  console.log({x, y, actualOffset})
+function Crosshair({ x, y, canvasSize }) {
   return (
     <>
       {/* Horizontal Line */}
-      <Line points={[actualOffset.x, actualOffset.y+y, actualOffset.x+width, actualOffset.y+y]} stroke="green" strokeWidth={1} />
+      <Line points={[x, 0, x, canvasSize]} stroke="green" strokeWidth={1} />
       {/* Vertical Line */}
-      <Line points={[actualOffset.x+x, actualOffset.y, actualOffset.x+x, actualOffset.y+height]} stroke="green" strokeWidth={1} />
+      <Line points={[0, y, canvasSize, y]} stroke="green" strokeWidth={1} />
     </>
   );
 }
