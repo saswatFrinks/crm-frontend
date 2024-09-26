6503c9a431f11e018a936133fee9e0cb8689389b
navendrafrinksai <navendra.agrawal@frinks.ai>
2024-09-26 15:12:44 +0530

    Commented incomplete buttons

--- src/shared/layouts/main/sidebar.jsx
+++ src/shared/layouts/main/sidebar.jsx
@@ -49,50 +49,50 @@ export default function Sidebar() {
         </svg>
       ),
     },
-    {
-      title: 'Analytics',
-      to: '/analytics',
-      pathname: '/analytics',
-      icon: (active) => (
-        <svg
-          width="14"
-          height="18"
-          viewBox="0 0 14 18"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M13 17V7M7 17V1M1 17V11"
-            stroke={`${active ? '#fff' : '#0E0F0F'}`}
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      ),
-    },
-    {
-      title: 'Lisence & Payments',
-      to: '/lisence-payments',
-      pathname: '/lisence-payments',
-      icon: (active) => (
-        <svg
-          width="14"
-          height="24"
-          viewBox="0 0 14 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M7 1V23M12 5H4.5C3.57174 5 2.6815 5.36875 2.02513 6.02513C1.36875 6.6815 1 7.57174 1 8.5C1 9.42826 1.36875 10.3185 2.02513 10.9749C2.6815 11.6313 3.57174 12 4.5 12H9.5C10.4283 12 11.3185 12.3687 11.9749 13.0251C12.6313 13.6815 13 14.5717 13 15.5C13 16.4283 12.6313 17.3185 11.9749 17.9749C11.3185 18.6313 10.4283 19 9.5 19H1"
-            stroke={`${active ? '#fff' : '#0E0F0F'}`}
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      ),
-    },
+    // {
+    //   title: 'Analytics',
+    //   to: '/analytics',
+    //   pathname: '/analytics',
+    //   icon: (active) => (
+    //     <svg
+    //       width="14"
+    //       height="18"
+    //       viewBox="0 0 14 18"
+    //       fill="none"
+    //       xmlns="http://www.w3.org/2000/svg"
+    //     >
+    //       <path
+    //         d="M13 17V7M7 17V1M1 17V11"
+    //         stroke={`${active ? '#fff' : '#0E0F0F'}`}
+    //         strokeWidth="1.5"
+    //         strokeLinecap="round"
+    //         strokeLinejoin="round"
+    //       />
+    //     </svg>
+    //   ),
+    // },
+    // {
+    //   title: 'Lisence & Payments',
+    //   to: '/lisence-payments',
+    //   pathname: '/lisence-payments',
+    //   icon: (active) => (
+    //     <svg
+    //       width="14"
+    //       height="24"
+    //       viewBox="0 0 14 24"
+    //       fill="none"
+    //       xmlns="http://www.w3.org/2000/svg"
+    //     >
+    //       <path
+    //         d="M7 1V23M12 5H4.5C3.57174 5 2.6815 5.36875 2.02513 6.02513C1.36875 6.6815 1 7.57174 1 8.5C1 9.42826 1.36875 10.3185 2.02513 10.9749C2.6815 11.6313 3.57174 12 4.5 12H9.5C10.4283 12 11.3185 12.3687 11.9749 13.0251C12.6313 13.6815 13 14.5717 13 15.5C13 16.4283 12.6313 17.3185 11.9749 17.9749C11.3185 18.6313 10.4283 19 9.5 19H1"
+    //         stroke={`${active ? '#fff' : '#0E0F0F'}`}
+    //         strokeWidth="1.5"
+    //         strokeLinecap="round"
+    //         strokeLinejoin="round"
+    //       />
+    //     </svg>
+    //   ),
+    // },
   ];
 
   return (
